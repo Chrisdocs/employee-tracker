999482ca86a878675f45135a3b4bd9277bf259a7
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-06-04 21:21:02 -0500

    captures users selection for later use

--- index.js
+++ index.js
@@ -13,6 +13,33 @@ const promptUser = () => {
 promptUser()
 //get the user selection
 .then(userSelection => {
-    console.log(userSelection.mainSelect);
+    
+    console.log(userSelection);
+
+    console.log('This returns "userSelection.mainSelect": ' + userSelection.mainSelect);
+
+    console.log('This returns "userSelection[mainSelect]": ' + userSelection['mainSelect']);
+
+    var selection = userSelection['mainSelect'];
+    console.log(selection);
+
+    if (selection === 'View All Departments') {
+        console.log('You Selected view all dept');
+    } else if (selection === 'View All Roles') {
+        console.log('You selected view all roles');
+    } else if (selection === 'View All Employees') {
+        console.log('You selected view all employees');
+    } else if (selection === 'Add A Department') {
+        console.log('You selected add a dept')
+    } else if (selection === 'Add A Role') {
+        console.log('You selected Add A Role')
+    } else if (selection === 'Add An Employee') {
+        console.log('You selected Add An Employee')
+    } else if (selection === 'Update An Employees Role') {
+        console.log('You selected Update An Employees Role')
+    } else {
+        console.log('You did not select a valid option.')
+    }
+
 })
 //retireve relative data from sql server
